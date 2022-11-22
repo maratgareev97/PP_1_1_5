1179f5bf361dfc3a15bd255dd853783802f4eeb8
gareev_mr <maratgareev97@gmail.com>
2022-11-22 13:59:14 +0300

    Dao Service 20.11

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -20,7 +20,6 @@ public class UserServiceImpl implements UserService {
 
     public void createUsersTable() {
 //        userDao.createUsersTable();
-
         userDaoHibernate.createUsersTable();
     }
 
